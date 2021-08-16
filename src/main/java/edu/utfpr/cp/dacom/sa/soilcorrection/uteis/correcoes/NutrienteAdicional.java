3fec29472d797245e062cabf25a5dc5eb7857484
Loran Braga <loranpb@gmail.com>
2021-08-16 11:25:00 -0300

    fix: retirado decoration denescessario

--- src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/uteis/correcoes/NutrienteAdicional.java
+++ src/main/java/edu/utfpr/cp/dacom/sa/soilcorrection/uteis/correcoes/NutrienteAdicional.java
@@ -4,17 +4,14 @@ import edu.utfpr.cp.dacom.sa.soilcorrection.enums.NomeNutrienteAdicional;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import lombok.NonNull;
 
 @RequiredArgsConstructor
 @Setter
 @Getter
 public class NutrienteAdicional {
 
-    @NonNull
     private final NomeNutrienteAdicional nome;
 
-    @NonNull
     private final double teorNutriente;
 
     private double correcaoAdicional;    
